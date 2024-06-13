dab59cd75cf7fac73ace93b0826b07827a58b06d
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-06-13 17:55:03 +0200

    [UPD] AppContactJumbo

---
+++ src/components/AppContactJumbo.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name:'AppContactJumbo',
+}
+
+</script>
+
+<template>
+    <section id="jumbotron">
+        <div class="container-fluid p-5 text-center " >
+            <h2 class="text-white position-absolute top-50 start-50 translate-middle fs-1" id="about-content">
+                Contact            
+            </h2>
+        </div>
+    </section>
+
+</template>
+
+<style scoped lang="scss">
+@import '../assets/scss/partials/_variables.scss';
+
+@keyframes fadeIn {
+    0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+#about-content {
+  animation: fadeIn 2s;
+  letter-spacing: .15em;
+}
+
+#jumbotron .container-fluid {
+    max-width: 100%;
+    background-image: url(../img/banner-14@2x-scaled.jpg);
+    background-size: cover;
+    background-color: antiquewhite;
+    background-color: #c4c3c3;
+    background-blend-mode: multiply;
+    height: 500px;
+}
+
+</style>
--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -1,15 +1,18 @@
 <script>
-import AppStaff from '../components/AppStaff.vue'
+import AppContactJumbo from '../components/AppContactJumbo.vue'
+import AppContactForm from'../components/AppContactForm.vue';
 import AppTestimonial from '../components/AppTestimonial.vue';
 import AppNewsLetter from '../components/AppNewsLetter.vue';
-import AppContactForm from'../components/AppContactForm.vue';
+import AppStaff from '../components/AppStaff.vue';
+
 export default{
     name: 'AppContact',
     components: {
-        AppStaff,
-        AppTestimonial,
-        AppNewsLetter,
-        AppContactForm,
+      AppContactJumbo,
+      AppContactForm,
+      AppTestimonial,
+      AppNewsLetter,
+      AppStaff,   
     }
 }
 </script>
@@ -17,13 +20,7 @@ export default{
 <template>
 
     <!----Inizio Sezione Jumbotron-->
-    <section id="jumbotron">
-        <div class="container-fluid p-5 text-center " >
-            <h2 class="text-white position-absolute top-50 start-50 translate-middle fs-1" id="about-content">
-                Contact            
-            </h2>
-        </div>
-    </section>
+    <AppContactJumbo />
     <!----Fine Sezione Jumbotron-->
     
     <!----Inizio Sezione Forms-->
@@ -45,86 +42,5 @@ export default{
 </template>
 
 <style scoped lang="scss">
-@import '../assets/scss/partials/_variables.scss';
 
-@keyframes fadeIn {
-    0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-
-#about-content {
-  animation: fadeIn 2s;
-  letter-spacing: .15em;
-}
-
-#jumbotron .container-fluid {
-    max-width: 100%;
-    background-image: url(../img/banner-14@2x-scaled.jpg);
-    background-size: cover;
-    background-color: antiquewhite;
-    background-color: #c4c3c3;
-    background-blend-mode: multiply;
-    height: 500px;
-     //filter: brightness(65%);
-     
-     
-     }
-
-
-     /* spiegheremo cosa fanno queste classi in seguito!*/
-
-
-.button-color{
-  background-color: $button-h4-icon-color;
-}
-
-.green-color{
-    color: $button-h4-icon-color;
-}
-.button-color:hover{
-  background-color: $button-bg-hover;
-}
-
-
-#banner-header {
-    /* uso bacground color in common.scss bg-main */
-    background-color: $main-color;
-    background-image: url('../img/bg-transparent-3.png'); 
-    background-repeat: repeat-x;
-   
-  }
-
-  .col-12 h4{
-    text-transform: uppercase;
-    color: white;
-
-  }
-
-  .t-up{
-    text-transform:uppercase;
-    color: white;
-  }
-
-  .testimonial img{
-    width: 100px;
-  }
-
-  
-
-  .staff img{
-    width: 100%;
-  }
-
-  .scale:hover {
-       transform: scale(1.03);
-       filter: brightness(1.05);
-       transition-duration: 0.4s; 
-       }
-
-
-  
 </style>
